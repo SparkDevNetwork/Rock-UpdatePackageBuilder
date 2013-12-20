371c6d070a52133e088a8ba387f5944e6997b87e
Nick Airdo <nairdo@gmail.com>
2013-12-20 15:25:34 -0700

    uses command line options library and other fixes.

--- RockPackageBuilder/BuildUpdatePackage.cs
+++ RockPackageBuilder/BuildUpdatePackage.cs
@@ -18,9 +18,9 @@ namespace RockPackageBuilder
         //static string PACKAGE_ROOT_SAVE_PATH = @"C:\Misc\_NuGetLocal";
         //static string REPO_PATH = @"C:\Misc\Rock-ChMS";
         //static string REPO_BRANCH = "develop";
-        static string LAST_PACKAGE_COMMIT_SHA = "f336fbdcc7a59c3f216a4bf27fb11ae2cf4d41e8";
+        static string LAST_PACKAGE_COMMIT_SHA = "794515bf0f9995b243beda8385bdb24333e8cac2"; // 794515bf0f9995b243beda8385bdb24333e8cac2 is version 0.0.2
 
-        static List<string> NON_WEB_PROJECTS = new List<string> { "rock", "rock.rest", "rock.version" };
+        static List<string> NON_WEB_PROJECTS = new List<string> { "rock", "rock.migrations", "rock.rest", "rock.version" };
         #endregion
 
         // Define a class to receive parsed values
@@ -29,7 +29,7 @@ namespace RockPackageBuilder
             [Option( 's', "saveCurrentCommitSHA", DefaultValue = @"false", HelpText = "Set to true to store the last commit SHA as the starting point for the next run." )]
             public string SaveCurrentCommitSHA { get; set; }
 
-            [Option( 'b', "repoBranch", DefaultValue = @"develop", HelpText = "The branch to operate against when performing package builds." )]
+            [Option( 'b', "repoBranch", DefaultValue = @"master", HelpText = "The branch to operate against when performing package builds." )]
             public string RepoBranch { get; set; }
 
             [Option( 'r', "repoPath", DefaultValue = @"C:\Misc\Rock-ChMS", HelpText = "The path to your local git repository." )]
@@ -37,6 +37,9 @@ namespace RockPackageBuilder
 
             [Option( 'p', "packageFolder", DefaultValue = @"C:\Misc\_NuGetLocal", HelpText = "The folder to put the output package." )]
             public string PackageFolder { get; set; }
+
+            [Option( 'v', "verbose", DefaultValue = false, HelpText = "Set to true to see a more verbose output of what's changed in the repo." )]
+            public bool Verbose { get; set; }
 
             [ParserState]
             public IParserState LastParserState { get; set; }
@@ -50,6 +53,7 @@ namespace RockPackageBuilder
 
         static int Main( string[] args )
         {
+            Console.BufferHeight = 9999;
             var options = new Options();
             var parser = new CommandLine.Parser( with => with.HelpWriter = Console.Error );
             if ( parser.ParseArgumentsStrict( args, options, () => Environment.Exit( -2 ) ) )
@@ -78,14 +82,14 @@ namespace RockPackageBuilder
             List<string> deletedPackageFiles = new List<string>();
             Dictionary<string, bool> modifiedProjects = new Dictionary<string, bool>();
 
-            //TODO -- update the Rock.Version project's version # and then commit->push
+            //TODO -- make sure the Rock.Version project's version number has been updated and commit->pushed
             //        before you do the rest.
 
-            GetRockWebChangedFilesAndProjects( options.RepoPath, options.RepoBranch, modifiedPackageFiles, deletedPackageFiles, modifiedProjects );
+            GetRockWebChangedFilesAndProjects( options, modifiedPackageFiles, deletedPackageFiles, modifiedProjects );
 
             // TODO determine how to increment versions
             // TODO determine where to get description from
-            BuildPackage( options.RepoPath, options.PackageFolder, modifiedPackageFiles, "0.0.0", "a description" );
+            BuildPackage( options.RepoPath, options.PackageFolder, modifiedPackageFiles, deletedPackageFiles, modifiedProjects, "0.0.4", "various changes" );
 
             // TODO create "RockChMS" wrapper package as per: https://github.com/SparkDevNetwork/Rock-ChMS/wiki/Packaging-Rock-Core-Updates
 
@@ -101,14 +105,14 @@ namespace RockPackageBuilder
         /// <param name="modifiedPackageFiles">a list of files that were modified in the RockWeb project</param>
         /// <param name="deletedPackageFiles">a list of files that were deleted from the RockWeb project</param>
         /// <param name="modifiedProjects">a list of projects that were modified</param>
-        private static void GetRockWebChangedFilesAndProjects( string repoPath, string repoBranch, List<string> modifiedPackageFiles, List<string> deletedPackageFiles, Dictionary<string, bool> modifiedProjects )
+        private static void GetRockWebChangedFilesAndProjects( Options options, List<string> modifiedPackageFiles, List<string> deletedPackageFiles, Dictionary<string, bool> modifiedProjects )
         {
             int webRootPathLength = @"rockweb\".Length;
 
             // Open the git repo and get the commits for the given branch.
-            using ( var repo = new Repository( repoPath ) )
+            using ( var repo = new Repository( options.RepoPath ) )
             {
-                Branch branch = repo.Branches[ repoBranch ];
+                Branch branch = repo.Branches[ options.RepoBranch ];
 
                 var commits = branch.Commits;
 
@@ -128,24 +132,39 @@ namespace RockPackageBuilder
                         // skip a bunch of known projects we don't care about...
                         if ( file.Path.ToLower().EndsWith( ".gitignore" ) || file.Path.StartsWith( @"Apps\" ) ||
                             file.Path.StartsWith( @"Dev Tools\" ) || file.Path.StartsWith( @"Documentation\" ) ||
-                            file.Path.StartsWith( @"Rock Installer\" ) || file.Path.StartsWith( @"Rock.CodeGeneration\" ) ||
+                            file.Path.StartsWith( @"RockInstaller\" ) || file.Path.StartsWith( @"Rock Installer\" ) ||
+                            file.Path.StartsWith( @"Rock.CodeGeneration\" ) || file.Path.StartsWith( @"libs\" ) || file.Path.StartsWith( @"packages\" ) ||
                             file.Path.StartsWith( @"RockJobSchedulerService\" ) || file.Path.StartsWith( @"RockJobSchedulerServiceInstaller\" ) )
                         {
                             continue;
                         }
 
                         string projectName = file.Path.Split( Path.DirectorySeparatorChar ).First();
-                        Console.WriteLine( "{0}\t{1}", file.Path, file.Status );
+                        if ( options.Verbose )
+                        {
+                            Console.WriteLine( "{0}\t{1}", file.Path, file.Status );
+                        }
 
                         // any changes with any other non-RockWeb projects?
-                        if ( NON_WEB_PROJECTS.Contains( projectName ) )
+                        if ( NON_WEB_PROJECTS.Contains( projectName.ToLower() ) )
                         {
                             modifiedProjects[projectName] = true;
                         }
                         else if ( file.Path.ToLower().StartsWith( @"rockweb\bin\" ) ) // && x.Path.ToLower().EndsWith( ".dll" ) )
                         {
                             Console.WriteLine( "{0}\t{1} (ASSEMBLY)", file.Path, file.Status );
-                            modifiedPackageFiles.Add( file.Path );
+                            //modifiedPackageFiles.Add( file.Path );
+
+                            if ( file.Status == ChangeKind.Added || file.Status == ChangeKind.Modified )
+                            {
+                                var filePathSuffix = file.Path.Substring( webRootPathLength );
+                                //modifiedPackageFiles.Add( filePathSuffix );
+                                Console.WriteLine( string.Format( "NOTE: {0} DLL changed since last build. You need add it to the lib folder.", filePathSuffix ) );
+                            }
+                            else if ( file.Status == ChangeKind.Deleted )
+                            {
+                                deletedPackageFiles.Add( file.Path );
+                            }
                         }
                         else if ( file.Path.ToLower().StartsWith( @"rockweb\" ) )
                         {
@@ -164,25 +183,51 @@ namespace RockPackageBuilder
             }
         }
 
-        public static void BuildPackage( string repoPath, string packageFolder, List<string> packageFiles, string version, string description )
+        public static void BuildPackage( string repoPath, string packageFolder, List<string> packageFiles, List<string> deletedPackageFiles, Dictionary<string, bool> modifiedProjects, string version, string description )
         {
+            string dashVersion = version.Replace( '.', '-' );
+
             // Create a manifest for this package...
             Manifest manifest = new Manifest();
             manifest.Metadata = new ManifestMetadata()
             {
                 Authors = "SparkDevelopmentNetwork",
                 Version = version,
-                Id = "RockUpdate-" + version,
+                Id = "RockUpdate-" + dashVersion,
                 Description = description,
             };
+
             manifest.Files = new List<ManifestFile>();
             string webRootPath = Path.Combine( repoPath, "RockWeb" );
             foreach ( string file in packageFiles )
             {
+                // Skip the root web.config
+                if ( file.ToLower() == "web.config" )
+                {
+                    Console.WriteLine( "WARNING: web.config file was changed since last build." );
+                    continue;
+                }
+                
                 AddToManifest( manifest, file, webRootPath );
             }
 
-            string packageFileName = Path.Combine( packageFolder, string.Format( "RockUpdate-{0}.nupkg", version ) );
+            // write out all the files to delete in the deletefile.lst ) 
+            string deleteFileRelativePath = Path.Combine( "App_Data", "deletefile.lst" );
+            string deleteFileFullPath = Path.Combine( webRootPath, deleteFileRelativePath );
+            if ( deletedPackageFiles.Count > 0 )
+            {
+                using ( StreamWriter w = File.AppendText( deleteFileFullPath ) )
+                {
+                    foreach ( string delete in deletedPackageFiles )
+                    {
+                        w.WriteLine( delete );
+                    }
+                }
+
+                AddToManifest( manifest, deleteFileRelativePath, webRootPath );
+            }
+
+            string packageFileName = Path.Combine( packageFolder, string.Format( "RockUpdate-{0}.{1}.nupkg", dashVersion, version ) );
 
             PackageBuilder builder = new PackageBuilder();
             builder.PopulateFiles( repoPath, manifest.Files );
@@ -190,6 +235,11 @@ namespace RockPackageBuilder
             using ( FileStream stream = File.Open( packageFileName, FileMode.OpenOrCreate ) )
             {
                 builder.Save( stream );
+            }
+
+            foreach( KeyValuePair<string, bool> entry in modifiedProjects )
+            {
+                Console.WriteLine( string.Format( "NOTE: {0} project files were changed since last build. You need to build and add them to the lib folder.", entry.Key ) );
             }
         }
 
